d7ebdf60ac1de60a68e40da1c81939028fa17e73
黃威智 <huangweizhi@huangweizhideMacBook-Pro.local>
2020-09-29 00:53:25 +0800

    Revert "swap redis module"
    This reverts commit 194fb5f8a9f5c98881f1513f9b9419df27a2b0f8.

--- train.js
+++ train.js
@@ -1,7 +1,7 @@
 const io = require('socket.io-client');
 
-const redis = require('redis');
-const client = redis.createClient();
+const asyncRedis = require("async-redis");
+const client = asyncRedis.createClient();
 
 const { reduce, toNumber, max, random, map } = require('lodash');
 
